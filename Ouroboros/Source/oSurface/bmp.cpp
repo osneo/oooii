5012df39f54f17104f0e8a1d74ddc695f8012017
arciuolo <arciuolo@gmail.com>
2014-08-04 06:05:07 +0000

    Support v3/4/5 bmps

--- Ouroboros/Source/oSurface/bmp.cpp
+++ Ouroboros/Source/oSurface/bmp.cpp
@@ -46,49 +46,45 @@ info get_info_bmp(const void* buffer, size_t size)
 scoped_allocation encode_bmp(const texel_buffer& b, const alpha_option& option, const compression& compression)
 {
 	auto info = b.get_info();
-
 	oCHECK(info.format == surface::format::b8g8r8a8_unorm || info.format == surface::format::b8g8r8_unorm, "source must be b8g8r8a8_unorm or b8g8r8_unorm");
+	const uint src_byte_count = element_size(info.format);
+	const auto format = alpha_option_format(info.format, option);
 
-	const uint ElementSize = surface::element_size(info.format);
+	const uint ElementSize = surface::element_size(format);
 	const uint UnalignedPitch = ElementSize * info.dimensions.x;
 	const uint AlignedPitch = byte_align(UnalignedPitch, 4);
 	const uint BufferSize = AlignedPitch * info.dimensions.y;
 
-	const uint bfOffBits = sizeof(bmp_header) + sizeof(bmp_info);
-	const uint bfSize = sizeof(bmp_header) + sizeof(bmp_info) + BufferSize;
+	const uint bfOffBits = sizeof(bmp_header) + sizeof(bmp_infoheader);
+	const uint bfSize = bfOffBits + BufferSize;
 
 	scoped_allocation p(malloc(bfSize), bfSize, free);
 
 	auto bfh = (bmp_header*)p;
 	auto bmi = (bmp_info*)&bfh[1];
-	void* bits = &bmi[1];
+	void* bits = byte_add((void*)p, bfOffBits);
 
-	bfh->bfType = 0x4d42; // 'BM'
+	bfh->bfType = bmp_signature;
+	bfh->bfSize = bfSize;
 	bfh->bfReserved1 = 0;
 	bfh->bfReserved2 = 0;
-	bfh->bfSize = bfSize;
 	bfh->bfOffBits = bfOffBits;
 	
 	bmi->bmiHeader.biSize = sizeof(bmp_infoheader);
 	bmi->bmiHeader.biWidth = info.dimensions.x;
 	bmi->bmiHeader.biHeight = info.dimensions.y;
 	bmi->bmiHeader.biPlanes = 1;
-	bmi->bmiHeader.biBitCount = info.format == surface::format::b8g8r8a8_unorm ? 32 : 24;
-	bmi->bmiHeader.biCompression = 0; // BI_RGB
+	bmi->bmiHeader.biBitCount = format == surface::format::b8g8r8a8_unorm ? 32 : 24;
+	bmi->bmiHeader.biCompression = bmp_compression::rgb;
 	bmi->bmiHeader.biSizeImage = BufferSize;
 	bmi->bmiHeader.biXPelsPerMeter = 0x0ec4;
 	bmi->bmiHeader.biYPelsPerMeter = 0x0ec4;
 	bmi->bmiHeader.biClrUsed = 0;
 	bmi->bmiHeader.biClrImportant = 0;
-	bmi->bmiColors[0].rgbBlue = 0;
-	bmi->bmiColors[0].rgbGreen = 0;
-	bmi->bmiColors[0].rgbRed = 0;
-	bmi->bmiColors[0].rgbReserved = 0;
 
 	shared_lock lock(b);
 	
 	uint Padding = AlignedPitch - UnalignedPitch;
-
 	for (uint y = 0, y1 = info.dimensions.y-1; y < info.dimensions.y; y++, y1--)
 	{
 		uchar* scanline = (uchar*)byte_add(bits, y * AlignedPitch);
@@ -100,11 +96,11 @@ scoped_allocation encode_bmp(const texel_buffer& b, const alpha_option& option, 
 			*scanline++ = src[2];
 			if (bmi->bmiHeader.biBitCount == 32)
 				*scanline++ = src[3];
-			src += (bmi->bmiHeader.biBitCount >> 3);
+			src += src_byte_count;
 		}
 
 		for (uint x = UnalignedPitch; x < AlignedPitch; x++)
-			*scanline = 0;
+			*scanline++ = 0;
 	}
 
 	return p;
@@ -114,7 +110,7 @@ texel_buffer decode_bmp(const void* buffer, size_t size, const alpha_option& opt
 {
 	const bmp_header* bfh = (const bmp_header*)buffer;
 	const bmp_info* bmi = (const bmp_info*)&bfh[1];
-	const void* bits = &bmi[1];
+	const void* bits = byte_add(buffer, bfh->bfOffBits);
 
 	info si = get_info_bmp(buffer, size);
 	oCHECK(si.format != format::unknown, "invalid bmp");
--- Ouroboros/Source/oSurface/bmp.h
+++ Ouroboros/Source/oSurface/bmp.h
@@ -30,12 +30,36 @@
 
 static const uint16_t bmp_signature = 0x4d42; // 'BM'
 
+enum bmp_compression
+{
+	rgb = 0,
+	rle8 = 1,
+	rle4 = 2,
+	bitfields = 3,
+	jpeg = 4,
+	png = 5,
+};
+
 struct bmp_rgbquad
 {
 	uint8_t rgbBlue;
 	uint8_t rgbGreen;
 	uint8_t rgbRed;
 	uint8_t rgbReserved;
+};
+
+struct ciexyz
+{
+	int32_t ciexyzX;
+	int32_t ciexyzY;
+	int32_t ciexyzZ;
+};
+
+struct ciexyztriple
+{
+  ciexyz ciexyzRed;
+  ciexyz ciexyzGreen;
+  ciexyz ciexyzBlue;
 };
 
 struct bmp_infoheader
@@ -51,6 +75,58 @@ struct bmp_infoheader
 	int32_t biYPelsPerMeter;
 	uint32_t biClrUsed;
 	uint32_t biClrImportant;
+};
+
+struct bmp_v4infoheader
+{
+	uint32_t bV4Size;
+	int32_t bV4Width;
+	int32_t bV4Height;
+	uint16_t bV4Planes;
+	uint16_t bV4BitCount;
+	uint32_t bV4V4Compression;
+	uint32_t bV4SizeImage;
+	int32_t bV4XPelsPerMeter;
+	int32_t bV4YPelsPerMeter;
+	uint32_t bV4ClrUsed;
+	uint32_t bV4ClrImportant;
+	uint32_t bV4RedMask;
+	uint32_t bV4GreenMask;
+	uint32_t bV4BlueMask;
+	uint32_t bV4AlphaMask;
+	uint32_t bV4CSType;
+	ciexyztriple bV4Endpoints;
+	uint32_t bV4GammaRed;
+	uint32_t bV4GammaGreen;
+	uint32_t bV4GammaBlue;
+};
+
+struct bmp_v5infoheader
+{
+	uint32_t bv5Size;
+	int32_t bv5Width;
+	int32_t bv5Height;
+	uint16_t bv5Planes;
+	uint16_t bv5BitCount;
+	uint32_t bv5v5Compression;
+	uint32_t bv5SizeImage;
+	int32_t bv5XPelsPerMeter;
+	int32_t bv5YPelsPerMeter;
+	uint32_t bv5ClrUsed;
+	uint32_t bv5ClrImportant;
+	uint32_t bv5RedMask;
+	uint32_t bv5GreenMask;
+	uint32_t bv5BlueMask;
+	uint32_t bv5AlphaMask;
+	uint32_t bv5CSType;
+	ciexyztriple bv5Endpoints;
+	uint32_t bv5GammaRed;
+	uint32_t bv5GammaGreen;
+	uint32_t bv5GammaBlue;
+	uint32_t bv5Intent;
+	uint32_t bv5ProfileData;
+	uint32_t bv5ProfileSize;
+	uint32_t bv5Reserved;
 };
 
 struct bmp_info
@@ -69,5 +145,11 @@ struct bmp_header
 	uint32_t bfOffBits;
 };
 #pragma pack(pop)
+
+static_assert(sizeof(bmp_infoheader) == 40, "size mismatch");
+static_assert(sizeof(bmp_v4infoheader) == 108, "size mismatch");
+static_assert(sizeof(bmp_v5infoheader) == 124, "size mismatch");
+static_assert(sizeof(bmp_info) == 44, "size mismatch");
+static_assert(sizeof(bmp_header) == 14, "size mismatch");
 
 #endif
