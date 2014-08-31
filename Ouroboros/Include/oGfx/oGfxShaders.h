7d3c901de5467aa95d47f5514a5f77d087e02594
arciuolo <arciuolo@gmail.com>
2014-08-31 19:37:16 +0000

    fix bad merge

--- Ouroboros/Include/oGfx/oGfxShaders.h
+++ Ouroboros/Include/oGfx/oGfxShaders.h
@@ -1,87 +1,87 @@
 // Copyright (c) 2014 Antony Arciuolo. See License.txt regarding use.
-// Encapsulate compiled shader code into a Caccessible form.
+#ifndef oGfxShaders_h
+#define oGfxShaders_h
 
-+ACM-ifndef oGfxShaders+AF8-h
-+ACM-define oGfxShaders+AF8-h
+// Encapsulate compiled shader code into a C++-accessible form.
 
-+ACM-include +ADw-oMesh/mesh.h+AD4-
+#include <oMesh/mesh.h>
 
-namespace ouro +AHs-
-	namespace gfx +AHs-
+namespace ouro {
+	namespace gfx {
 
-namespace gfx+AF8-vl
-+AHs-	enum value : uchar +AHs-
+namespace gfx_vl
+{	enum value : uchar {
 	
 	null,
 	count,
 
-+AH0AOwB9-
+};}
 
-namespace gfx+AF8-vs
-+AHs-	enum value : uchar +AHs-
-
-	null,
-	count,
-
-+AH0AOwB9-
-
-namespace gfx+AF8-hs
-+AHs-	enum value : uchar +AHs-
+namespace gfx_vs
+{	enum value : uchar {
 
 	null,
 	count,
 
-+AH0AOwB9-
+};}
 
-namespace gfx+AF8-ds
-+AHs-	enum value : uchar +AHs-
-
-	null,
-	count,
-
-+AH0AOwB9-
-
-namespace gfx+AF8-gs
-+AHs-	enum value : uchar +AHs-
+namespace gfx_hs
+{	enum value : uchar {
 
 	null,
 	count,
 
-+AH0AOwB9-
+};}
 
-namespace gfx+AF8-ps
-+AHs-	enum value : uchar +AHs-
-
-	null,
-	count,
-
-+AH0AOwB9-
-
-namespace gfx+AF8-cs
-+AHs-	enum value : uchar +AHs-
+namespace gfx_ds
+{	enum value : uchar {
 
 	null,
 	count,
 
-+AH0AOwB9-
+};}
+
+namespace gfx_gs
+{	enum value : uchar {
+
+	null,
+	count,
+
+};}
+
+namespace gfx_ps
+{	enum value : uchar {
+
+	null,
+	count,
+
+};}
+
+namespace gfx_cs
+{	enum value : uchar {
+
+	null,
+	count,
+
+};}
 
 // returns the elements of input
-mesh::element+AF8-array elements(const gfx+AF8-vl::value+ACY- input)+ADs-
+mesh::element_array elements(const gfx_vl::value& input);
 
 // returns the vertex shader byte code with the same input
 // signature as input.
-const void+ACo- vs+AF8-byte+AF8-code(const gfx+AF8-vl::value+ACY- input)+ADs-
+const void* vs_byte_code(const gfx_vl::value& input);
 
 // returns the buffer of bytecode compiled during executable compilation time
 // (not runtime compilation)
-const void+ACo- byte+AF8-code(const gfx+AF8-vs::value+ACY- shader)+ADs-
-const void+ACo- byte+AF8-code(const gfx+AF8-hs::value+ACY- shader)+ADs-
-const void+ACo- byte+AF8-code(const gfx+AF8-ds::value+ACY- shader)+ADs-
-const void+ACo- byte+AF8-code(const gfx+AF8-gs::value+ACY- shader)+ADs-
-const void+ACo- byte+AF8-code(const gfx+AF8-ps::value+ACY- shader)+ADs-
-const void+ACo- byte+AF8-code(const gfx+AF8-cs::value+ACY- shader)+ADs-
+const void* byte_code(const gfx_vs::value& shader);
+const void* byte_code(const gfx_hs::value& shader);
+const void* byte_code(const gfx_ds::value& shader);
+const void* byte_code(const gfx_gs::value& shader);
+const void* byte_code(const gfx_ps::value& shader);
+const void* byte_code(const gfx_cs::value& shader);
 
-	+AH0- // namespace gfx
-+AH0- // namespace ouro
+	} // namespace gfx
+} // namespace ouro
 
-+ACM-endif
+#endif
